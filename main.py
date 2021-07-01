643a459e4256293b3830276044ff742c6b8680b8
peppermatt94 <matteo.peperoni@gmail.com>
2021-07-01 17:19:58 +0200

    The idea take form: a CLI that use yfinance database to forecast and simulate new data

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 24 18:22:21 2021
-
-@author: pepermatt94
 
 
 Ideas for the following program:
@@ -19,11 +16,11 @@ import pandas as pd
 import seaborn as sns
 import pylab as plt
 import scipy.stats as st
-import numpy.random as rnd
+
 
 
 def euler_maruyama(a,b,x0,T):    #Integration of the black scholes equation: ITO process
-    '''Integration of the black scholes equation: ITO process
+    '''Integration of the black-scholes equation: ITO process
     
         Parameters
         ----------
@@ -38,25 +35,34 @@ def euler_maruyama(a,b,x0,T):    #Integration of the black scholes equation: ITO
             
         Returns
         -------
-        A step furhter in the ITO process
+        ITO simulation from start time to T
         '''
     N = len(T)
-    x = np.zeros((N,len(x0)))
+    x = np.zeros((N,len(T)))
     x[0] = x0
     for i in range(N-1):
         dt = T[i+1]-T[i]
         dWt = np.random.normal(0,dt)
         x[i+1] = x[i] + a(T[i],x[i])*dt + b(T[i],x[i])*dWt
     return x
- 
+
 Starting_File = "file with data analyzed with snakemake"
 
 volatility = "something_get_from_data"
 Expected_Rate_Of_Return =  "something_else_get_from_data"
 
-T = 100
+T = np.arange(0,100,1)
 dt = "someting"                                        #"small period of time"
-epsilon = rnd.randn(T)                #"Wiener variable"
 
-#dSOverS = Expected_Rate_Of_Return*dt +volatility*epsilon*np.sqrt(dt)
+a = lambda x,t : 0.2
+b = lambda x,t: 0.9
+x0=100
 
+x = euler_maruyama(a,b,x0,T)
+plt.plot(T,x)
+
+#ITO lemma 
+import requests
+
+a = requests.get("https://pkgstore.datahub.io/core/natural-gas/daily_json/data/2e630ca50c39a1a3cf6c3aff57a1b132/daily_json.json")
+print(a.content)
