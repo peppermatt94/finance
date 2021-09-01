07c2cbfc4c466aed6514be4495d674a2231d7b4e
peppermatt94 <matteo.peperoni@gmail.com>
2021-09-01 15:27:19 +0200

    some change in requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'finance',
     version = '0.1.0',
     packages = ['finance'],
-    install_requires = ["matplotlib", "pandas", "numpy", "yfinance","scipy"],
+    install_requires = ["matplotlib", "pandas", "numpy", "yfinance","scipy", "tabulate", "hypothesis"],
     entry_points = {
         'console_scripts': [
             'finance = finance.__main__:main',
