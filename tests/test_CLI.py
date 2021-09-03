940703ca46f9d0b5890e88b5883d4f7185e5a3aa
peppermatt94 <matteo.peperoni@gmail.com>
2021-09-03 12:27:42 +0200

    more atomic structure in test

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -10,52 +10,99 @@ import os
 
 class CLI_Tests(unittest.TestCase):
     #@unittest.skip("demonstrated")
-    def test_principle_commands_work(self):
+    def test_BMcommand_work(self):
+        '''
+    Given :
+        - command to perform BM simulation output for 100 days.
+    And :
+        - assert the program execute without problem (exit 0)'
+    '''    
         BM_output = subprocess.run("finance --company msft --output 'simulation.csv' ito --BM 100 ", stdout=subprocess.DEVNULL)
-        GBM_output = subprocess.run("finance --company msft --output 'simulation.csv' ito --GBM 100 ", stdout=subprocess.DEVNULL)
-        levy_output = subprocess.run("finance --company msft --output 'simulation.csv' ito --levy 100", stdout=subprocess.DEVNULL)
-        graphix_stock_output = subprocess.run("finance --input 'simulation.csv' graphix --stocks", stdout=subprocess.DEVNULL, capture_output=False)
-        graphix_dreturn_output = subprocess.run("finance --input 'simulation.csv' graphix --dReturns", stdout=subprocess.DEVNULL,capture_output=False)
-        self.assertEqual( [BM_output.returncode, GBM_output.returncode, levy_output.returncode, graphix_stock_output.returncode, graphix_dreturn_output.returncode], [0, 0, 0, 0, 0])
+        self.assertEqual(BM_output.returncode, 0)
         
-    @unittest.skip("demonstrated")
-    @given(data_frames(index=indexes(elements=st.datetimes(min_value=pd.Timestamp(2019, 1, 1),
-            max_value=pd.Timestamp(2020, 9, 1)),  min_size=15, unique=True),
-            columns=[column("Close", elements = st.floats( allow_nan = True, allow_infinity=False), dtype=float),
-            column("longName", elements = st.text(alphabet = 'p',min_size = 2, max_size = 2))]))
-    @settings(deadline=None)
-    def test_drop_nan(self,x):
-        x = x.sort_index()
-        x[x.Close<0.1] = 0.1   # i put some upper and lower limit on the data created
-        x[x.Close>1000] = 1000
-        x.longName = "prova"
-        x.to_csv("simulation.csv")
-        code_output = subprocess.run("finance --input simulation.csv ito --BM 100", stdout=subprocess.DEVNULL)
-        self.assertEqual( code_output.returncode, 0)
+    def test_GBMcommand_work(self):
+        '''
+    Given :
+        - command to perform GBM simulation output for 100 days.
+    And :
+        - assert the program execute without problem (exit 0)'
+    '''    
+        GBM_output = subprocess.run("finance --company msft --output 'simulation.csv' ito --GBM 100 ", stdout=subprocess.DEVNULL)
+        self.assertEqual( GBM_output.returncode,0)
+        
+    def test_levycommand_work(self):
+        '''
+    Given :
+        - command to perform levy simulation output for 100 days.
+    And :
+        - assert the program execute without problem (exit 0)'
+    '''    
+        levy_output = subprocess.run("finance --company msft --output 'simulation.csv' ito --levy 100", stdout=subprocess.DEVNULL)
+        self.assertEqual( levy_output.returncode, 0)
     
+    def test_graphix_stockcommand_work(self):
+        '''
+    Given :
+        - command to perform stock graph.
+    And :
+        - assert the program execute without problem (exit 0)'
+    '''    
+        graphix_stock_output = subprocess.run("finance --input 'simulation.csv' graphix --stocks", stdout=subprocess.DEVNULL, capture_output=False)
+        self.assertEqual( graphix_stock_output.returncode, 0)
+   
+    def test_graphix_dReturnscommands_work(self):
+        '''
+    Given :
+        - command to perform the daily return graph.
+    And :
+        - assert the program execute without problem (exit 0)'
+    '''    
+        graphix_dreturn_output = subprocess.run("finance --input 'simulation.csv' graphix --dReturns", stdout=subprocess.DEVNULL,capture_output=False)
+        self.assertEqual(  graphix_dreturn_output.returncode, 0)
+         
     #@unittest.skip("demonstrated")    
     def test_tick_list_folder_independence(self):
+        '''
+    Given :
+        - program ran list_of_company command in different folders.
+    And :
+        - assert the  program execute without problem (exit 0)'
+    '''    
         folder1 = subprocess.run("finance --list_of_companies", stdout=subprocess.DEVNULL)
         os.chdir("C:\\")
         folder2 = subprocess.run("finance --list_of_companies",stdout=subprocess.DEVNULL)
         self.assertEqual([folder1.returncode,folder2.returncode], [0,0])
         
     def test_output_file_correctly_created(self):
+        '''
+    Given :
+        - program create an output file in file.csv.
+    And :
+        - assert the  program create the file in current folder'
+    '''    
         file = 'file.csv'
         subprocess.run(f"finance --output {file}", stdout=subprocess.DEVNULL)
-        if os.path.exists(file):
-            database = pd.read_csv(file)   
-            columns = database.columns
-            columns = columns.to_list()
+        self.assertTrue(os.path.exists(file))
+        database = pd.read_csv(file)   
+        columns = database.columns
+        columns = columns.to_list()
         self.assertEqual(columns, ['Date','Open','High','Low','Close','Volume','Dividends','Stock Splits','longName'])
         
+     
     def test_output_file_ito_simulation_correctly_created(self):
+        '''
+    Given :
+        - program create an output file of the ito simulation .
+    And :
+        - assert the program create the file correcly in the current folder
+        - assert the program create the file with well formatted columns'
+    '''    
         file = 'file.csv'
         subprocess.run(f"finance --company msft --output {file} ito --BM 100", stdout=subprocess.DEVNULL)
-        if os.path.exists(file):
-            database = pd.read_csv(file)   
-            columns = database.columns
-            columns = columns.to_list()     
+        self.assertTrue(os.path.exists(file))
+        database = pd.read_csv(file)   
+        columns = database.columns
+        columns = columns.to_list()     
         self.assertEqual(columns, ['Unnamed: 0','Close','longName'])
         
     def test_logging_file_created_in_script_dir(self):
